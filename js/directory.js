e1afc1249052936bc253a6601850375ad0d5623f
Frederick Gibbs <fwgibbs@gmail.com>
2017-04-16 15:24:59 -0400

    path

--- js/directory.js
+++ js/directory.js
@@ -50,7 +50,7 @@ function createCard(i,page) {
     console.log("processing " + filename);
 
     var rowString = '<a href="'+filename+'">';
-    rowString += '<div class="card">';
+    rowString += '<div class="card">'; 
     rowString += '<img class="card-img-top" src="images/cards/' + page.image + '.jpg" alt="Essay image">';
     rowString += '<div class="card-block">';
     rowString += '<h4 class="card-title">' + page.title + '</h4>';
